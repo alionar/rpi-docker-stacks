bf81f32655673dfb6b2b176da67ebd41ea2ed88c
Aulia L <alionar.13@gmail.com>
2020-06-14 13:27:54 +0700

    remove debug and prod subpath

--- selenium/debug/build/build.sh
+++ selenium/debug/build/build.sh
@@ -1,2 +1,2 @@
-docker build -t rpi666/rpi-selenium-node-firefox-debug ./debug/node-firefox-debug \
-    && docker build -t  rpi666/rpi-selenium-standalone-firefox-debug ./debug/standalone-firefox-debug
+docker build -t rpi666/rpi-selenium-node-firefox-debug ./node-firefox-debug \
+    && docker build -t  rpi666/rpi-selenium-standalone-firefox-debug ./standalone-firefox-debug
--- selenium/prod/build/build.sh
+++ selenium/prod/build/build.sh
@@ -1,2 +1,2 @@
-docker build -t rpi666/rpi-selenium-node-firefox ./prod/node-firefox \
-    && docker build -t  rpi666/rpi-selenium-standalone-firefox ./prod/standalone-firefox
+docker build -t rpi666/rpi-selenium-node-firefox ./node-firefox \
+    && docker build -t  rpi666/rpi-selenium-standalone-firefox ./standalone-firefox
